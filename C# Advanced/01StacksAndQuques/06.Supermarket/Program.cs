7eef01d06063dd23d2507baff7a3b952c29bd7c8
SimeonKovachev <skcodingacc@gmail.com>
2024-01-30 21:43:16 +0200

    Fix

---
+++ C# Advanced/01StacksAndQuques/06.Supermarket/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Queue<string> strings = new Queue<string>();
+
+string command = Console.ReadLine();
+
+while(command != "End")
+{
+    if(command == "Paid")
+    {
+        while (strings.Count > 0)
+        {
+            string name = strings.Dequeue();
+            Console.WriteLine(name);
+        }
+    }
+    else
+    {
+      strings.Enqueue(command);
+    }
+    command = Console.ReadLine();
+}
+
+Console.WriteLine($"{strings.Count} people remaining.");
